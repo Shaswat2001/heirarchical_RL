f83a5664d5660f6d9320877fba41d6ae6d4f1709
Shaswat2001 <sis_shaswat@outlook.com>
2025-06-09 12:20:21 -0400

    feat: finish goal conditioned dataset

--- utils/buffers.py
+++ utils/buffers.py
@@ -1,6 +1,6 @@
 import numpy as np
 from typing import Any
-    
+
 import jax 
 from flax.core.frozen_dict import FrozenDict
 
@@ -48,4 +48,82 @@ class GCDataset:
     dataset: Dataset
     config: Any
 
+    def __post_init__(self):
+
+        self.size = self.dataset.size
+
+        self.where_terminate = np.nonzero(self.dataset["terminated"] > 0)
+
+        assert self.where_terminate[-1] == self.size - 1
+
+        assert np.isclose(
+            self.config['value_p_curgoal'] + self.config['value_p_trajgoal'] + self.config['value_p_randomgoal'], 1.0
+        )
+        assert np.isclose(
+            self.config['actor_p_curgoal'] + self.config['actor_p_trajgoal'] + self.config['actor_p_randomgoal'], 1.0
+        )
+
+    def sample(self, batch_size, idxs = None):
+
+        if idxs is None:
+            idxs = self.dataset.get_random_idxs(batch_size)
+        
+        batch = self.dataset.sample(batch_size, idxs)
+
+        value_goal_idxs = self.sample_goals(
+            idxs,
+            self.config['value_p_curgoal'],
+            self.config['value_p_trajgoal'],
+            self.config['value_p_randomgoal'],
+            self.config['value_geom_sample'],
+        )
+        actor_goal_idxs = self.sample_goals(
+            idxs,
+            self.config['actor_p_curgoal'],
+            self.config['actor_p_trajgoal'],
+            self.config['actor_p_randomgoal'],
+            self.config['actor_geom_sample'],
+        )
+
+        batch['value_goals'] = self.get_observations(value_goal_idxs)
+        batch['actor_goals'] = self.get_observations(actor_goal_idxs)
+        successes = (idxs == value_goal_idxs).astype(float)
+        batch['masks'] = 1.0 - successes
+        batch['rewards'] = successes - (1.0 if self.config['gc_negative'] else 0.0)
+
+        return batch
+
+    def sample_goals(self, idxs, p_curgoal, p_trajgoal, p_randomgoal, geom_sample):
+        """Sample goals for the given indices."""
+        batch_size = len(idxs)
+
+        # Random goals.
+        random_goal_idxs = self.dataset.get_random_idxs(batch_size)
+
+        # Goals from the same trajectory (excluding the current state, unless it is the final state).
+        final_state_idxs = self.where_terminate[np.searchsorted(self.where_terminate, idxs)]
+        if geom_sample:
+            # Geometric sampling.
+            offsets = np.random.geometric(p=1 - self.config['discount'], size=batch_size)  # in [1, inf)
+            traj_goal_idxs = np.minimum(idxs + offsets, final_state_idxs)
+        else:
+            # Uniform sampling.
+            distances = np.random.rand(batch_size)  # in [0, 1)
+            traj_goal_idxs = np.round(
+                (np.minimum(idxs + 1, final_state_idxs) * distances + final_state_idxs * (1 - distances))
+            ).astype(int)
+        if p_curgoal == 1.0:
+            goal_idxs = idxs
+        else:
+            goal_idxs = np.where(
+                np.random.rand(batch_size) < p_trajgoal / (1.0 - p_curgoal), traj_goal_idxs, random_goal_idxs
+            )
+
+            # Goals at the current state.
+            goal_idxs = np.where(np.random.rand(batch_size) < p_curgoal, idxs, goal_idxs)
+
+        return goal_idxs
+    
+    def get_observations(self, idxs):
+        return jax.tree.map(lambda arr: arr[idxs], self.dataset['observations'])
 
