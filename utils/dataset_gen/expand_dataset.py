de08df1cf6fa07f8e4cc3b410f158b27c778010a
Shaswat2001 <sis_shaswat@outlook.com>
2025-07-06 00:08:54 -0400

    fix: create new dataset

--- utils/dataset_gen/expand_dataset.py
+++ utils/dataset_gen/expand_dataset.py
@@ -11,7 +11,7 @@ def main(args):
     data_path = f'{dir_name}/dataset/FrankaGolfCourseEnv-v0/train/FrankaGolfCourseEnv-v0_train_augmented.npz'
     ep = np.load(data_path, allow_pickle=True)
     ep = {key: ep[key] for key in ep}
-    env = gym.make("FrankaGolfCourseEnv-v0", keyframe="random", render_mode="human")
+    env = gym.make("FrankaGolfCourseEnv-v0", keyframe="random")
     def deterministic_int_generator(seed, low, high, count=1):
         rng = random.Random(seed)  # Create a local Random instance
         return [rng.randint(low, high) for _ in range(count)]
@@ -32,6 +32,7 @@ def main(args):
     ep["goal_idxs"] = np.array([7, 16, 27, 36, 42, 56, 187], np.int32)
     seeds = deterministic_int_generator(888, 1, 60000000, 100)
     print(seeds)
+    applied_seeds = []
     for i in range(100):
         observation, info = env.reset(seed=seeds[i])
 
@@ -41,18 +42,20 @@ def main(args):
         new_obs = []
         new_actions = []
         new_dones = []
-        for i in range(interp_obs.shape[0]):
+        for j in range(interp_obs.shape[0]):
             new_obs.append(observation)
-            action = list(interp_obs[i][:7]) + [1.0]
+            action = list(interp_obs[j][:7]) + [1.0]
             new_actions.append(action)
             new_dones.append(False)
 
             action[-1] = 255
             observation, reward, terminated, truncated, info = env.step(action)
+            print
+            if terminated:
+                break
 
-        keep = input("Do you want to keep this trajectory")
-
-        if keep == "Y":
+        if not terminated:
+            applied_seeds.append(seeds[i])
             new_obs = np.array(new_obs, np.float32)
             new_actions = np.array(new_actions, np.float32)
             new_dones = np.array(new_dones, bool)
@@ -80,7 +83,9 @@ def main(args):
                 terminals=ep["terminals"],
             )
 
-            print(new_obs.shape)    
+            print(new_obs.shape)   
+
+    print(applied_seeds) 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
